583dae351da179b8aa7543b448124fa1fc162d67
Marcus Martins <marcusjava89@gmail.com>
2025-07-07 15:34:33 -0300

    Iniciando projeto sistema-clientes-java.

---
+++ src/main/java/com/sistemaclliente/SistemaClientesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaclliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClientesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClientesJavaApplication.class, args);
+	}
+
+}
