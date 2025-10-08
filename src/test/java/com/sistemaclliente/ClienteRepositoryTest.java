4f0549deb7f56206aaf29059021d61e94af87fef
Marcus Martins <marcusjava89@gmail.com>
2025-10-08 13:33:55 -0300

    Testa save de ClienteRepository

--- src/test/java/com/sistemaclliente/ClienteRepositoryTest.java
+++ src/test/java/com/sistemaclliente/ClienteRepositoryTest.java
@@ -36,7 +36,25 @@ public class ClienteRepositoryTest {
 		
 		assertThat(encontrado1).isPresent();		
 		assertThat(encontrado1.get().getNome()).isEqualTo("Marcus");		
-		assertThat(encontrado1.get().getId()).isEqualTo(1L);		
+		assertThat(encontrado1.get().getId()).isEqualTo(cliente1.getId());		
+		assertThat(encontrado1.get().getCpf()).isEqualTo("23501206586");		
+		assertThat(encontrado1.get().getEmail()).isEqualTo("marcus@gmail.com");
+		
+	}
+	@Test
+	public void save_sucesso_salvaProduto() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("Marcus");
+		cliente1.setCpf("23501206586");
+		cliente1.setEmail("marcus@gmail.com");
+		
+		repository.save(cliente1);
+		
+		Optional<Cliente> encontrado1 = repository.findById(cliente1.getId());
+		
+		assertThat(encontrado1).isPresent();		
+		assertThat(encontrado1.get().getNome()).isEqualTo("Marcus");		
+		assertThat(encontrado1.get().getId()).isEqualTo(cliente1.getId());		
 		assertThat(encontrado1.get().getCpf()).isEqualTo("23501206586");		
 		assertThat(encontrado1.get().getEmail()).isEqualTo("marcus@gmail.com");
 		
