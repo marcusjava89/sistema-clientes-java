985ef693ef79fb24b8d92c552845366367dca15e
Marcus Martins <marcusjava89@gmail.com>
2025-10-10 12:39:29 -0300

    Refatora ClienteRepositoryTest

--- src/test/java/com/sistemaclliente/ClienteRepositoryTest.java
+++ src/test/java/com/sistemaclliente/ClienteRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.sistemaclliente;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,29 +23,47 @@ public class ClienteRepositoryTest {
 	@Autowired
 	private ClienteRepository repository;
 	
+	@BeforeEach
+	public void setup() {
+		repository.deleteAll();
+		
+		Cliente cliente1 = clienteNovo("Marcus", "23501206586", "marcus@gmail.com");
+		Cliente cliente2 = clienteNovo("Antonio", "20219064674", "antonio@gmail.com");
+		repository.saveAndFlush(cliente1);
+		repository.saveAndFlush(cliente2);
+	}
+	
+	private Cliente clienteNovo(String nome, String cpf, String email) {
+		Cliente cliente = new Cliente(); /*id gerado pelo banco.*/
+		cliente.setNome(nome);
+		cliente.setCpf(cpf);
+		cliente.setEmail(email);
+		
+		return cliente;
+	}
+	
 	@Test
 	public void contextLoad() {}
 	
 	@Test
 	public void saveAndFlush_sucesso_salvaProdutoDeImediato() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setNome("Marcus");
-		cliente1.setCpf("23501206586");
-		cliente1.setEmail("marcus@gmail.com");
-		/*O banco de dados decide como salvar o id.*/
-		repository.saveAndFlush(cliente1);
+		repository.deleteAll();
 		
-		Optional<Cliente> encontrado1 = repository.findById(cliente1.getId());
+		Cliente clienteGerado = clienteNovo("Vinicius", "54879652365", "vinicius@email.com");
+		repository.saveAndFlush(clienteGerado);
 		
-		assertThat(encontrado1).isPresent();		
-		assertThat(encontrado1.get().getNome()).isEqualTo("Marcus");		
-		assertThat(encontrado1.get().getId()).isEqualTo(cliente1.getId());		
-		assertThat(encontrado1.get().getCpf()).isEqualTo("23501206586");		
-		assertThat(encontrado1.get().getEmail()).isEqualTo("marcus@gmail.com");
+		Optional<Cliente> clienteEncontrado = repository.findById(clienteGerado.getId());
+		
+		assertThat(clienteEncontrado).isPresent();
+		assertThat(clienteEncontrado.get().getNome()).isEqualTo("Vinicius");
+		assertThat(clienteEncontrado.get().getCpf()).isEqualTo("54879652365");
+		assertThat(clienteEncontrado.get().getEmail()).isEqualTo("vinicius@email.com");
 	}
 	
 	@Test
 	public void save_sucesso_salvaProdutoNoBanco() {
+		repository.deleteAll();
+		
 		Cliente cliente1 = new Cliente();
 		cliente1.setNome("Marcus");
 		cliente1.setCpf("23501206586");
@@ -61,20 +81,7 @@ public class ClienteRepositoryTest {
 	}
 	
 	@Test
-	public void findAll_listaCheia() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setNome("Marcus");
-		cliente1.setCpf("23501206586");
-		cliente1.setEmail("marcus@gmail.com");
-
-		Cliente cliente2 = new Cliente();
-		cliente2.setNome("Antonio");
-		cliente2.setCpf("20219064674");
-		cliente2.setEmail("antonio@gmail.com");
-		
-		repository.saveAndFlush(cliente1);
-		repository.saveAndFlush(cliente2);
-		
+	public void findAll_listaCheia() {	
 		List<Cliente> listaClientes = repository.findAll();
 		
 		assertThat(listaClientes).isNotNull();
@@ -94,10 +101,21 @@ public class ClienteRepositoryTest {
 	@Test
 	public void findAll_listaVazia() {
 		repository.deleteAll();
+		
 		List<Cliente> listaClientes = repository.findAll();
 		
 		assertThat(listaClientes).isNotNull();
 		assertThat(listaClientes).isEmpty();
+	}
+	
+	@Test
+	public void findByCpf_retornaCliente(){		
+		Optional<Cliente> clienteResgatado = repository.findByCpf("23501206586");
+		
+		assertThat(clienteResgatado).isPresent();
+		assertThat(clienteResgatado.get().getNome()).isEqualTo("Marcus");
+		assertThat(clienteResgatado.get().getCpf()).isEqualTo("23501206586");
+		assertThat(clienteResgatado.get().getEmail()).isEqualTo("marcus@gmail.com");
 	}
 	
 }
